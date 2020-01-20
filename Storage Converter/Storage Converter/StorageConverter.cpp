247b5381bc02aacb5c81ab8263d0d54b9fbd1c0e
crypticle <35649100+crypticle@users.noreply.github.com>
2020-01-20 11:49:13 -0500

    Added newly created project.
    * included identification and program description comments.
    * Added main().

---
+++ Storage Converter/Storage Converter/StorageConverter.cpp
@@ -0,0 +1,21 @@
+// Calvin Le
+// 1-20-2020
+// Digital Storage Converter:
+/*
+	A programm that asks the user to input an amount of physical storage (e.g., "500 GB") then convert the supplied number to all units of digital storage.
+	In addition, the program will generate the amount of bits needed to address the amount of storage entered by the user.
+*/
+
+// I have neither given nor received unauthorized aid in completing this work, nor have I presented someone else's work as my own. ~ Calvin Le
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+	// Parses the input string.
+
+}
+
